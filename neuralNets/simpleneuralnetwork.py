1863f9f198ade20dd2ee2ec927753e8f2f975081
Tim Ogden <togden1999@gmail.com>
2019-02-09 16:59:27 -0500

    Occassional commit

--- neuralNets/simpleneuralnetwork.py
+++ neuralNets/simpleneuralnetwork.py
@@ -17,11 +17,13 @@ class SimpleNeuralNetwork:
 		model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['accuracy'])
 		return model
 
-	def fit_model(self, batch_size=12, epochs=10, verbose=1):
+	def fit_model(self, batch_size=32, epochs=10, verbose=1):
 		# to do:
 		# (self.train_x, self.train_y) = get_data()
 		# (self.test_x, self.test_y) = get_data()
 		# (self.val_x, self.val_y) = get_data()
+		self.model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                    steps_per_epoch=len(x_train) / batch_size, epochs=epochs, verbose=verbose, validation_data=[test_x, test_y])
 		self.model.fit(x=train_x, y=train_y, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=[test_x, test_y])
 		self.model.save('SimpleNeuralNetwork-bs{}-ep{}'.format(batch_size,epochs) + '.h5')
 
