4f44a8bf5fa8a280663e1d00914f2174d9aaf445
John Lin <jlin1674@gmail.com>
2019-02-09 18:25:27 -0500

    nparray in obtain images

--- src/obtain_images.py
+++ src/obtain_images.py
@@ -1,8 +1,9 @@
 from file_loader import read_img_file_names
 import cv2
+import numpy as np
 from os.path import exists
 
-def obtain_data(filename, amount=-1):
+def obtain_data(filename, amount=-1, transform=lambda x: x):
     #names = n x 2 array
     names = read_img_file_names(filename)
 
@@ -16,10 +17,10 @@ def obtain_data(filename, amount=-1):
         short_fn = row[0]
         long_fn = row[1]
         if exists(short_fn) and exists(long_fn):
-            short_exposures.append(cv2.imread(short_fn, cv2.IMREAD_COLOR))
-            long_exposures.append(cv2.imread(long_fn, cv2.IMREAD_COLOR))
+            short_exposures.append(transform(cv2.imread(short_fn, cv2.IMREAD_COLOR)))
+            long_exposures.append(transform(cv2.imread(long_fn, cv2.IMREAD_COLOR)))
         i += 1
-    return (short_exposures, long_exposures)
+    return (np.array(short_exposures), np.array(long_exposures))
             
 if __name__ == '__main__':
-    obtain_data('../train.txt')
+    obtain_data('../train.txt', amount=32)
