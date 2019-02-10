cd669c83c60f960a6f4bd0575c9df1ee6253a929
John Lin <jlin1674@gmail.com>
2019-02-09 23:49:30 -0500

    new files and get batch

--- src/obtain_images.py
+++ src/obtain_images.py
@@ -16,12 +16,35 @@ def obtain_data(filename, amount=-1, transform=lambda x: x):
             break
         short_fn = row[0]
         long_fn = row[1]
-        if exists(short_fn) and exists(long_fn):
+        #if exists(short_fn) and exists(long_fn):
+        short_exposures.append(transform(cv2.imread(short_fn, cv2.IMREAD_COLOR)))
+        long_exposures.append(transform(cv2.imread(long_fn, cv2.IMREAD_COLOR)))
+        i += 1
+    return (np.array(short_exposures), np.array(long_exposures))
+         
+def get_length(file):
+    return sum(1 for line in open(file))
+
+def get_batch(filename, batch_index, batch_size, transform=lambda x: x):
+    names = read_img_file_names(filename)
+
+    short_exposures = []
+    long_exposures = []
+
+    i = 0
+    start_index = batch_index * batch_size
+    end_index = (batch_index + 1) * batch_size
+    for row in names:
+        if i >= end_index:
+            return (np.array(short_exposures), np.array(long_exposures))
+        if start_index <= i and i < end_index:
+            short_fn = row[0]
+            long_fn = row[1]
             short_exposures.append(transform(cv2.imread(short_fn, cv2.IMREAD_COLOR)))
             long_exposures.append(transform(cv2.imread(long_fn, cv2.IMREAD_COLOR)))
         i += 1
     return (np.array(short_exposures), np.array(long_exposures))
-            
+
 def shrink_func(xres, yres):
     return lambda x: cv2.resize(x, dsize=(xres, yres), interpolation=cv2.INTER_CUBIC)
 
@@ -35,4 +58,8 @@ def shrink_greyscale_func(xres, yres, nchannels):
         return shrink_func(xres, yres)
 
 if __name__ == '__main__':
-    obtain_data('../train.txt', amount=32)
+    #data = (obtain_data('../new_train.txt', amount=32))
+    batch = get_batch('../new_train.txt', 0, 32)
+    batch2 = get_batch('../new_train.txt', 1, 32)
+    print(batch[0].shape, batch[1].shape)
+    print(batch2[0].shape, batch2[1].shape)
--- src/restructure_files.py
+++ src/restructure_files.py
@@ -1,5 +1,6 @@
 import os
 import file_loader
+from os.path import exists
 
 def create_files():
     names = ['train', 'val', 'test']
@@ -20,9 +21,18 @@ def reorganize_files():
                 file_loader.copy_img(file_name, data_type[i], 'short' if not index else 'long', i)
             i += 1
         
+def remove_nonexistent_files():
+    txt_files = ['../train.txt', '../val.txt', '../test.txt']
+    new_txt_files = ['../new_train.txt', '../new_val.txt', '../new_test.txt']
+    for i, txt_file in enumerate(txt_files):
+        with open(txt_file) as fo:
+            with open(new_txt_files[i], 'w') as new_fo:
+                for line in fo:
+                    # get rid of new line in entry
+                    entry = line.split(' ')
+                    entry[1] = entry[1][:-1]
+                    if exists(entry[0]) and exists(entry[1]):
+                        new_fo.write(entry[0] + ' ' + entry[1] + '\n')
+
 if __name__ == '__main__':
-    try:
-        create_files()
-    except:
-        pass
-    reorganize_files()
+    remove_nonexistent_files()
