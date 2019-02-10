6afb6dfb57029d70f4f981717cf94894fbc1d48d
Tim Ogden <togden1999@gmail.com>
2019-02-10 05:47:17 -0500

    .

--- src/cloud_simplecnn.py
+++ src/cloud_simplecnn.py
@@ -4,7 +4,7 @@ from keras.models import load_model
 from obtain_images import *
 import numpy as np
 import obtain_images
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from keras import backend as K
 import math
 #from statistics import mean
@@ -31,50 +31,44 @@ class ConvolutionalNeuralNetwork:
 				Conv2D(64, (2,2), padding='same'),
 				MaxPooling2D((2, 2)),
 				Conv2D(128, (2, 2), padding='same'),
-				UpSampling2D((2, 2)),
+				UpSampling2D((2, 2)),	
+				Conv2D(64, (2, 2), padding='same'),
 				UpSampling2D((2, 2)),
 				Conv2D(self.n_channels, (2,2), padding='same')
 			])
 		model.compile(optimizer=keras.optimizers.Adam(lr=.00001), loss='mean_squared_error', metrics=['accuracy'])
 		return model
 
-        def fit_model(self, batch_size=1, epochs=10, verbose=1, amount=-1, track_losses=False):
+	def fit_model(self, batch_size=1, epochs=10, verbose=1, amount=-1, track_losses=False):
 		
 		# to do:
 		
-		num_batches = math.ceil(1863/batch_size)
-		losses = []
-		for epoch in range(self.init_epoch, epochs):
-			recent_losses = []
-			for batch_index in range(int(num_batches)):
-				(train_x_batch, train_y_batch) = get_batch('../new_train.txt', batch_index, batch_size, transform=shrink_greyscale_func(self.x_res,self.y_res,self.n_channels))
-				train_x_batch = self.preprocess(train_x_batch)
-				#print(train_x_batch.shape)
-				train_y_batch = self.preprocess(train_y_batch)
-				start = time.time()
-				loss = self.model.train_on_batch(x=train_x_batch, y=train_y_batch)
-				print('time {}'.format(time.time() - start))
-				train_y_batch = None
-				train_x_batch = None
-				print('Epoch {} of {}, batch {} of {}'.format(epoch+1,epochs,batch_index+1,num_batches))
-				print('Loss was {}'.format(loss[0]))
-				print('Accuracy was {}'.format(loss[1]))
-				recent_losses.append(loss[0])
-				losses.append(loss[0])
-			#print('Average loss of epoch {} was {}'.format(epoch+1, mean(recent_losses)))
-			
-			if epoch % 1 == 0:
-				self.model.save_weights('ConvolutionalNeuralNetwork-bs{}-ep{}-{}'.format(batch_size,epoch,epochs) + '.h5')
-			if epoch % 20 == 0:
-				if track_losses:
-                                    pass
-                                    '''
-                                    plt.plot(losses)
-                                    plt.show(block=False)
-                                    plt.pause(1)
-                                    plt.close()
-                                    '''
-                self.model.save('ConvolutionalNeuralNetwork-bs{}-ep{}-({},{})'.format(batch_size,epochs,self.x_res,self.y_res) + '.h5')
+			num_batches = math.ceil(1863/batch_size)
+			losses = []
+			for epoch in range(self.init_epoch, epochs):
+				recent_losses = []
+				for batch_index in range(int(num_batches)):
+					(train_x_batch, train_y_batch) = get_batch('../new_train.txt', batch_index, batch_size, transform=shrink_greyscale_func(self.x_res,self.y_res,self.n_channels))
+					train_x_batch = self.preprocess(train_x_batch)
+					#print(train_x_batch.shape)
+					train_y_batch = self.preprocess(train_y_batch)
+					start = time.time()
+					loss = self.model.train_on_batch(x=train_x_batch, y=train_y_batch)
+					print('time {}'.format(time.time() - start))
+					train_y_batch = None
+					train_x_batch = None
+					print('Epoch {} of {}, batch {} of {}'.format(epoch+1,epochs,batch_index+1,num_batches))
+					print('Loss was {}'.format(loss[0]))
+					print('Accuracy was {}'.format(loss[1]))
+					recent_losses.append(loss[0])
+					losses.append(loss[0])
+				#print('Average loss of epoch {} was {}'.format(epoch+1, mean(recent_losses)))
+				
+				if epoch % 1 == 0:
+					self.model.save_weights('ConvolutionalNeuralNetwork-bs{}-ep{}-{}'.format(batch_size,epoch,epochs) + '.h5')
+				if epoch % 20 == 0:
+					if track_losses:
+						pass
 
 	def test_model(self):
 		(val_x, val_y) = obtain_data('../val.txt', amount=-1, transform=shrink_greyscale_func(self.x_res,self.y_res,self.n_channels))
@@ -85,10 +79,7 @@ class ConvolutionalNeuralNetwork:
 
 	def predict(self, index):
 		(test_x, _) = obtain_data('../new_test.txt', amount=-1, transform=shrink_greyscale_func(self.x_res,self.y_res,self.n_channels))
-		#print(test_x)
-		
 
-                '''
 		plt.imshow(test_x[index], cmap='gray')
 		plt.show()
 		
@@ -101,7 +92,6 @@ class ConvolutionalNeuralNetwork:
 		img = img.reshape(self.x_res, self.y_res)
 		plt.imshow(img, cmap='gray')
 		plt.show()
-                '''
 
 	def __init__(self, x_res=1080, y_res=1616, n_channels=3):
 		self.x_res = x_res
@@ -110,18 +100,20 @@ class ConvolutionalNeuralNetwork:
 		self.model = self.build_model()
 		self.val_x = None
 		self.val_y = None
-                self.init_epoch = 0
+		self.init_epoch = 0
 
 	def load_model(self, name, epoch):
 		self.model.load_weights(name)
-                self.init_epoch = epoch
+		self.init_epoch = epoch
 
 if __name__ == '__main__':
 	neuralNet = ConvolutionalNeuralNetwork(x_res=128, y_res=128, n_channels=1)
-	#print(neuralNet.model.summary())
-        neuralNet.load_model('ConvolutionalNeuralNetwork-bs1000-ep20-1000.h5', epoch=20)
-	neuralNet.fit_model(amount=-1, batch_size=1000, epochs=1000, track_losses=True)
-	#neuralNet.load_model('ConvolutionalNeuralNetwork-bs32-ep5-(128,128).h5')
+
+	neuralNet.load_model('ConvolutionalNeuralNetwork-bs1000-ep20-1000.h5', epoch=20)
+	neuralNet.predict(index=23)
+	neuralNet.predict(index=5)
+	neuralNet.predict(index=11)
+	neuralNet.load_model('ConvolutionalNeuralNetwork-bs1000-ep32-1000.h5', epoch=35)
 	neuralNet.predict(index=23)
 	neuralNet.predict(index=5)
 	neuralNet.predict(index=11)
