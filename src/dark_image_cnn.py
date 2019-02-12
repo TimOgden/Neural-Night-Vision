2b5a9147e51459e4c91564cbffc2e765464a4bfa
Tim Ogden <togden1999@gmail.com>
2019-02-12 09:44:42 -0500

    Adding parallel gpu support to run on cloud

--- src/dark_image_cnn.py
+++ src/dark_image_cnn.py
@@ -3,7 +3,8 @@ import os
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
-K.tensorflow_backend._get_available_gpus()
+from keras.utils.training_utils import multi_gpu_model
+print('Available gpus:',K.tensorflow_backend._get_available_gpus())
 from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
 import math
 import tensorflow as tf
@@ -85,7 +86,7 @@ class Dark_Image_CNN:
 				y_train.append(lst[i][space+1:])
 			return x_train, y_train
 
-	def __init__(self, batch_size, epochs):
+	def __init__(self, batch_size, epochs, gpus=1):
 		self.init_datagens()
 		self.batch_size = batch_size
 		self.epochs = epochs
@@ -93,11 +94,13 @@ class Dark_Image_CNN:
 		self.x_res = 1080
 		self.y_res = 1616
 		self.n_channels = 3
+		self.gpus = gpus
 		self.model = self.build_model()
 		self.fit_model()
 if __name__=='__main__':
 	cnn = None
 	with tf.device('/cpu:0'):
-		cnn = Dark_Image_CNN(8,10)
-	cnn = multi_gpu_model(cnn,gpus=2)
+		cnn = Dark_Image_CNN(8,10, gpus=1)
+
+	cnn = multi_gpu_model(cnn,gpus=g)
 	cnn.fit_model()
