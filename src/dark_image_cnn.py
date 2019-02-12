915e6a12046f04d3c8c28abd7fcb390d4e91a134
Tim Ogden <togden1999@gmail.com>
2019-02-12 17:16:01 -0500

    Lots of bugs resolved

--- src/dark_image_cnn.py
+++ src/dark_image_cnn.py
@@ -11,6 +11,8 @@ import tensorflow as tf
 import cv2
 import numpy as np
 from statistics import mean
+import random
+
 class Dark_Image_CNN:
 	def init_datagens(self):
 		self.datagen_trainx = ImageDataGenerator(
@@ -35,8 +37,7 @@ class Dark_Image_CNN:
 			Conv2D(256, (3, 3), padding='same', activation='relu', kernel_initializer=keras.initializers.he_normal()),
 			UpSampling2D((2, 2)),
 			Conv2D(128, (3, 3), padding='same', activation='relu', kernel_initializer=keras.initializers.he_normal()),
-			UpSampling2D((2, 2)),	
-			Conv2D(64, (3, 3), padding='same', activation='relu', kernel_initializer=keras.initializers.he_normal()),
+			UpSampling2D((2, 2)),
 			Conv2D(self.n_channels, (1, 1), padding='same', activation='sigmoid', kernel_initializer=keras.initializers.he_normal())
 		])
 		model.compile(optimizer=keras.optimizers.Adam(lr=.0001, decay=1e-5), loss='mean_squared_error', metrics=['mean_squared_error'])
@@ -46,66 +47,69 @@ class Dark_Image_CNN:
 	def fit_model(self):
 		num_batches = math.ceil(1863/self.batch_size)
 		for epoch in range(self.epochs):
-			print('Epoch:', epoch+1)
-			batch_loss = []
 			for batch in range(num_batches):
+				percent = batch/num_batches*100
+				print('{:.1f} percent done with epoch {}'.format(percent,epoch+1))
 				x_train, y_train = self.get_batch()
-				print('original shapes:', x_train.shape, y_train.shape)
+				#print('original shapes:', x_train.shape, y_train.shape)
 				x_train.reshape(-1,self.x_res,self.y_res,self.n_channels)
 				#print('reshaped x_train:', x_train.shape)
 				y_train.reshape(-1,self.x_res,self.y_res,self.n_channels)
-				loss = self.model.fit_generator(self.datagen_trainx.flow(x_train, y_train), verbose=1)
-				batch_loss.append(loss[0])
+				self.model.fit_generator(self.datagen_trainx.flow(x_train, y_train), verbose=1)
+			
 			self.model.save('cnn-epoch{}'.format(epoch+1))
-			print('Epoch loss:',mean(batch_loss))
 
 
 
 	def get_batch(self):
-		line_num_start = self.current_batch_index
-		line_num_end = self.current_batch_index+self.batch_size
-		x_train, y_train = self.get_files(line_num_start,line_num_end)
 		img_x_train = []
 		img_y_train = []
+		x_train, y_train = self.get_files()
 		for i in range(len(x_train)):
 			try:
-				img_x = cv2.imread(x_train[i])
-				img_y = cv2.imread(y_train[i][:-1])  # y_train will have a \n or ' ' at end, so must remove it
-				
+				img_x = cv2.resize(cv2.imread(x_train[i]), (1616,1080))
+				img_y = cv2.resize(cv2.imread(y_train[i]), (1616,1080))
 				img_x_train.append(img_x)
 				img_y_train.append(img_y)
 			except IndexError as e:
 				pass
-
-		self.current_batch_index+=self.batch_size
 		return np.array(img_x_train), np.array(img_y_train)
 
-	def get_files(self, line_num_start, line_num_end):
+	def get_files(self):
 		with open('../new_train.txt','r') as f:
 			lst = f.readlines()
 			x_train = []
 			y_train = []
-			for i in range(line_num_start, line_num_end):
-				space = lst[i].index(' ')
-				x_train.append(lst[i][:space])
-				y_train.append(lst[i][space+1:])
+			for i in range(self.batch_size):
+				rand_index = random.randint(0,len(self.unused_files))
+				rand_el = self.unused_files[rand_index]
+				space = lst[rand_el].index(' ')
+				x_train.append(lst[rand_el][:space].strip())
+				y_train.append(lst[rand_el][space+1:].strip())
+				self.unused_files.remove(rand_el)	
 			return x_train, y_train
 
+	def get_file_len(self, file):
+		with open(file,'r') as f:
+			lst = f.readlines()
+			return len(lst)
+
 	def __init__(self, batch_size, epochs, gpus=1):
-		self.init_datagens()
 		self.batch_size = batch_size
+		self.unused_files = list(range(int(self.get_file_len('../new_train.txt'))))
 		self.epochs = epochs
 		self.current_batch_index = 0
 		self.x_res = 1080
 		self.y_res = 1616
 		self.n_channels = 3
 		self.gpus = gpus
+		self.init_datagens()
 		self.model = self.build_model()
-		self.fit_model()
+
 if __name__=='__main__':
 	cnn = None
 	with tf.device('/cpu:0'):
-		cnn = Dark_Image_CNN(8,10, gpus=1)
+		cnn = Dark_Image_CNN(4,10, gpus=1)
 
-	cnn = multi_gpu_model(cnn,gpus=g)
+	#cnn = multi_gpu_model(cnn,gpus=1)
 	cnn.fit_model()
