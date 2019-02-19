6499399699a368b7eeccf6c63654dee6579885c4
Tim Ogden <togden1999@gmail.com>
2019-02-18 20:59:42 -0500

    Fixed tensorboard, added checkpointing, trying to prevent overfittment

--- src/paper_cnn.py
+++ src/paper_cnn.py
@@ -3,7 +3,7 @@ import os
 import keras
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.callbacks import ModelCheckpoint, TensorBoard, LearningRateScheduler
 from keras.layers import Conv2D, Dropout, UpSampling2D, MaxPooling2D, LeakyReLU, Lambda
 import math
 import numpy as np
@@ -23,70 +23,71 @@ class Paper_CNN:
 				LeakyReLU(),
 				Conv2D(32, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 				MaxPooling2D((2,2), padding='same'),
 
 				Conv2D(64, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(64, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 				MaxPooling2D((2,2), padding='same'),
 
 				Conv2D(128, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(128, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 				MaxPooling2D((2,2), padding='same'),
 
 				Conv2D(256, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(256, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 				MaxPooling2D((2,2), padding='same'),
 
 				Conv2D(512, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(512, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 
 				UpSampling2D(),
 				Conv2D(256, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(256, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 
 				UpSampling2D(),
 				Conv2D(128, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(128, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 
 				UpSampling2D(),
 				Conv2D(64, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(64, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 
 				UpSampling2D(),
 				Conv2D(32, (3,3), padding='same'),
 				LeakyReLU(),
 				Conv2D(32, (3,3), padding='same'),
 				LeakyReLU(),
-				Dropout(.5),
+				Dropout(.3),
 
 				Conv2D(12, (1,1), padding='same', activation=None),
 				Lambda(self.depth_to_space)
 			])
-		self.callback = ModelCheckpoint('paper_model_weights.h5', monitor='val_loss', save_best_only=True, save_weights_only=True, verbose=0, mode='min')
-		self.callback2 = ModelCheckpoint('paper_model.h5', monitor='val_loss', save_best_only=True, verbose=1, mode='min')
-		self.tensorboard = TensorBoard(log_dir='./logs/', batch_size=128)
+		self.save_best = ModelCheckpoint('paper_model_weights.h5', monitor='val_loss', save_best_only=True, save_weights_only=True, verbose=1, mode='min')
+		self.checkpoint = ModelCheckpoint('paper_model_chkpt_{epoch:02d}.h5', monitor='val_loss', save_best_only=False, verbose=1, mode='min', period=5)
+		self.tensorboard = TensorBoard(log_dir='./logs/', batch_size=32)
+		self.lr_schedule = LearningRateScheduler(lr_schedule)
 		model.compile(optimizer=keras.optimizers.Adam(lr=.0001), loss='mean_absolute_error', metrics=['mean_absolute_error'])
 		print(model.summary())
 		return model
@@ -94,8 +95,9 @@ class Paper_CNN:
 
 	def fit_model(self, batch_size, epochs, initial_epoch):
 		self.model.fit_generator(self.generate_arrays_from_file('../new_train.txt'), 
-			steps_per_epoch=math.ceil(self.num_training_samples/batch_size), epochs=epochs, initial_epoch=initial_epoch,
-			validation_data=self.generate_arrays_from_file('../val.txt'), validation_steps=26, callbacks=[self.callback, self.callback2, self.tensorboard])
+			steps_per_epoch=math.ceil(self.num_training_samples/(batch_size/2)), epochs=epochs, initial_epoch=initial_epoch,
+			validation_data=self.generate_arrays_from_file('../val.txt'), validation_steps=26,
+			callbacks=[self.save_best, self.checkpoint, self.tensorboard, self.lr_schedule])
 
 	def load_model(self, file):
 		self.model = load_model(file)
@@ -109,7 +111,7 @@ class Paper_CNN:
 					x1, y = self.process_line(line)
 					if x1 is None or y is None:
 						continue
-					for x_batch, y_batch in self.datagen.flow(x1,y):
+					for x_batch, y_batch in self.datagen.flow(x1,y, shuffle=True):
 						yield ({'conv2d_1_input': x_batch}, {'lambda_1': y_batch})
 
 	def process_line(self,line):
@@ -132,6 +134,12 @@ class Paper_CNN:
 	def depth_to_space(self, input_tensor):
 		return tf.image.resize_bilinear(tf.depth_to_space(input_tensor, 2), (1080,1616))
 
+	def lr_sched(self, epoch):
+		if epoch<2000:
+			return 1e-4
+		else:
+			return 1e-5
+
 	def load_model(self, file):
 		self.model.load_weights(file)
 
@@ -141,13 +149,14 @@ class Paper_CNN:
 		self.n_channels = n_channels
 		self.num_training_samples = 1863
 		self.model = self.build_model()
-		self.datagen = ImageDataGenerator(horizontal_flip=True, rotation_range=20)
+		self.datagen = ImageDataGenerator(horizontal_flip=True, rotation_range=20, 
+			zoom_range=.2, vertical_flip=True, shear_range=3)
 
 if __name__=='__main__':
 	cnn = None
-	initial_epoch = 57
-	batch_size = 128
-	num_epochs = 2000
+	initial_epoch = 0
+	batch_size = 64
+	num_epochs = 4000
 	print(batch_size)
 
 	with tf.device('/cpu:0'):
