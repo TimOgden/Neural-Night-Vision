7d16c9eee252db8ddc9826101b85942997b83b97
Tim Ogden <togden1999@gmail.com>
2019-03-08 18:00:47 -0500

    Fully in black and white just for testing

--- src/paper_cnn.py
+++ src/paper_cnn.py
@@ -194,8 +194,8 @@ class Paper_CNN:
 		space = line.index(' ')
 		x_train = line[:space].strip()
 		y_train = line[space+1:].strip()
-		img_x = cv2.equalizeHist(cv2.imread(x_train))
-		img_y = cv2.imread(y_train)
+		img_x = cv2.equalizeHist(cv2.imread(x_train, 0))
+		img_y = cv2.imread(y_train, 0)
 		if img_x is None or img_y is None:
 			print('img x is none:', img_x is None, '\nimg y is none:', img_y is None)
 		img_x = cv2.resize(img_x, (1616,1080)) / 255.
