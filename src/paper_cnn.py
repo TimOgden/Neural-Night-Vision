496cca1b524cb2658cdf87671386c8c4449fe8fd
Tim Ogden <togden1999@gmail.com>
2019-03-19 15:37:57 -0400

    Switching to U-NET

--- src/paper_cnn.py
+++ src/paper_cnn.py
@@ -318,14 +318,14 @@ class Paper_CNN:
 		self.lr_schedule = LearningRateScheduler(self.lr_sched)
 
 if __name__=='__main__':
-	cnn = Paper_CNN(1080, 1616, 3, 'reg_model')
+	cnn = Paper_CNN(1080, 1616, 3, 'unet')
 
 	initial_epoch = 0
 	batch_size = 64
 	num_epochs = 4000
 	print(batch_size)
 
-	cnn.model = cnn.build_model()
+	cnn.model = cnn.build_unet()
 	print(cnn.model.summary())
 	if initial_epoch is not 0:
 		cnn.load_model('./weights/paper_model_chkpt_04.h5')
