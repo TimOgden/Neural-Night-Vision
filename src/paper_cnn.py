75b75dbd501a821439d23f6a0767f17f4f04103b
Tim Ogden <togden1999@gmail.com>
2019-03-18 22:40:06 -0400

    Added U-NET

--- src/paper_cnn.py
+++ src/paper_cnn.py
@@ -22,6 +22,58 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
 
 class Paper_CNN:
 
+	def build_unet(pretrained_weights=None, input_size=(self.x_res, self.y_res, self.n_channels)):
+		inputs = Input(input_size)
+		conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv1a')(inputs)
+		conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv1b')(conv1)
+		pool1 = MaxPooling2D(pool_size=(2, 2), name='pool1')(conv1)
+		conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv2a')(pool1)
+		conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv2b')(conv2)
+		pool2 = MaxPooling2D(pool_size=(2, 2), name='pool2')(conv2)
+		conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv3a')(pool2)
+		conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv3b')(conv3)
+		pool3 = MaxPooling2D(pool_size=(2, 2), name='pool3')(conv3)
+		conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv4a')(pool3)
+		conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv4b')(conv4)
+		drop4 = Dropout(0.5, name='drop4')(conv4)
+		pool4 = MaxPooling2D(pool_size=(2, 2), name='pool4')(drop4)
+
+		conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv5a')(pool4)
+		conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv5b')(conv5)
+		drop5 = Dropout(0.5, name='drop5')(conv5)
+
+		up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv6')(UpSampling2D(size = (2,2), name='up1')(drop5))
+		merge6 = concatenate([drop4,up6], axis = 3)
+		conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv7a')(merge6)
+		conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal', name='conv7b')(conv6)
+
+		up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+		merge7 = concatenate([conv3,up7], axis = 3)
+		conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+		conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+		up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+		merge8 = concatenate([conv2,up8], axis = 3)
+		conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+		conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+		up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+		merge9 = concatenate([conv1,up9], axis = 3)
+		conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+		conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+		conv10 = Conv2D(3, 1, activation = 'sigmoid')(conv9)
+
+		model = Model(input = inputs, output = conv10)
+
+		model.compile(optimizer = Adam(lr=1e-4, decay=1e-5), loss = 'mean_absolute_error')
+		
+		#model.summary()
+
+		if(pretrained_weights):
+			model.load_weights(pretrained_weights)
+
+		return model
+
 	def build_model(self):
 		dropout = 0
 		model = keras.Sequential([
@@ -176,7 +228,7 @@ class Paper_CNN:
 					if x1 is None or y is None:
 						continue
 					for x_batch, y_batch in self.val_datagen.flow(x1,y, shuffle=True):
-						yield ({'conv2d_1_input': x_batch}, {'lambda_1': y_batch})
+						yield ({'input_1': x_batch}, {'conv2d_20': y_batch})
 
 	def generate_val_from_file(self, path):
 		# Validation data generator
