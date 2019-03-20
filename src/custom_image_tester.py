ab53e85f4e7a915164f128b5970db7eb281723f9
Tim Ogden <togden1999@gmail.com>
2019-03-19 23:03:26 -0400

    Refined streaming function and made first video output

--- src/custom_image_tester.py
+++ src/custom_image_tester.py
@@ -5,12 +5,9 @@ from paper_cnn import Paper_CNN
 import time
 from cv2 import VideoCapture
 
-def load_image(path):
+def load_image(model, path):
 	img = cv2.imread(path)
 	img = cv2.resize(img, (1616,1080))
-	model = Paper_CNN(1080,1616,3, 'paper_model')
-	model.model = model.build_model()
-	model.load_model('./weights/reg_model_best.h5')
 
 	plt.subplot(1,2,1)
 	plt.imshow(img)
@@ -25,15 +22,11 @@ def load_image(path):
 	plt.title('CNN Prediction')
 	plt.show()
 
-def take_pic():
+def take_pic(model):
 	cam = VideoCapture(0)
 	s, img = cam.read()
 	if s:
 		img = cv2.resize(img, (1616,1080))
-		model = Paper_CNN(1080,1616,3, 'paper_model')
-		model.model = model.build_model()
-		model.load_model('./weights/reg_model_best.h5')
-
 		plt.subplot(1,2,1)
 		plt.imshow(img)
 		plt.title('Original')
@@ -49,6 +42,45 @@ def take_pic():
 	else:
 		print('Couldn\'t take picture!')
 
+def stream(model, recordingTime = 10):
+	cam = VideoCapture(0)
+	# Check if camera opened successfully
+	if (cam.isOpened() == False): 
+		print("Unable to read camera feed")
+	 
+	# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+	out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 15, (1616,1080))
+	orig = cv2.VideoWriter('orig.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 15, (1616,1080))
+	start = time.time()
+
+	while(True):
+		ret, frame = cam.read()
+		if ret == True:
+			frame = cv2.resize(frame, (1616, 1080))
+			orig.write(frame)
+			# Write the frame into the file 'output.avi'
+			frame = np.reshape(frame, (1, 1080, 1616, 3))/255.
+			yhat = model.predict(frame)
+			yhat = np.uint8(255 * yhat)
+			yhat = np.reshape(yhat, (1080, 1616, 3))
+			out.write(yhat)
+		
+			if time.time() - start >= recordingTime:
+				break
+	
+		# Break the loop
+		else:
+			break 
+	# When everything done, release the video capture and video write objects
+	cam.release()
+	out.release()
+	 
+	# Closes all the frames
+	cv2.destroyAllWindows()
 if __name__=='__main__':
-	#load_image('../Sony/test.jpg')
-	take_pic()
+	model = Paper_CNN(1080,1616,3, 'paper_model')
+	model.model = model.build_model()
+	model.load_model('./weights/reg_model_best.h5')
+	#load_image(model, '../Sony/test.jpg')
+	#take_pic(model)
+	stream(model, recordingTime = 30)
